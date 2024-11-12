a79229de20091ee0d32c1f70d061580894ead2a6
arabadjieff <alex_arabadjiev@abv.bg>
2024-11-12 10:48:45 +0200

    Update and rename final to wowGuessingGame.java

--- wowGuessingGame.java
+++ wowGuessingGame.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class wowGuessingGame {
     public static void main(String[] args) {
         Random rand = new Random();
         int randomNumber = rand.nextInt(100) + 1;
